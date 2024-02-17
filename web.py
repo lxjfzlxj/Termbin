29dd1187ae8148fc8a6aad645c29bf096ef67e62
LCrossingJ <936641716@qq.com>
2024-02-17 11:16:38 +0800

    use MySQL database to storage data

--- web.py
+++ web.py
@@ -3,12 +3,12 @@ from flask_restful import Resource, Api
 from datetime import datetime
 import hashlib
 import uuid
+from models import add, Clipboard, AuthorStatus, get_session
 
 app = Flask(__name__)
 api = Api(app)
 name = 'http://127.0.0.1:18080'
-    
-data = {}
+
 
 def get_char(num):
     if num < 26:
@@ -34,14 +34,23 @@ def get_short(id):
 
 class CreateResource(Resource):
     def post(self):
-        bytes = request.files['c'].read()
-        print('[Debug]Create a new clipboard, content: %s' % bytes.decode())
-        md5 = hashlib.md5()
-        md5.update(bytes)
-        id = str(uuid.uuid4())
-        short = get_short(id)
-        data[id] = dict(date = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f %Z'), digest = md5.hexdigest(), short = short, size = len(bytes), url = name + '/' + short, status = 'created', uuid = id, content = bytes.decode())
-        return make_response('''
+        with get_session() as session:
+            bytes = request.files['c'].read()
+            print('[Debug] Create a new clipboard, content: %s' % bytes.decode())
+            md5 = hashlib.md5()
+            md5.update(bytes)
+            id = str(uuid.uuid4())
+            short = get_short(id)
+            new_board = Clipboard(date = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f %Z'), digest = md5.hexdigest(), short = short, size = len(bytes), url = name + '/' + short, uuid = id, content = bytes.decode(), status = AuthorStatus.all)
+            try:
+                session.add(new_board)
+                session.commit()
+            except BaseException as e:
+                print('[Error] %s' % e)
+                status = 'failed'
+            else:
+                status = 'created'
+            return make_response('''
 date: %s
 digest: %s
 short: %s
@@ -49,37 +58,44 @@ size: %d
 url: %s
 status: %s
 uuid: %s
-'''
-% (data[id]['date'], data[id]['digest'], data[id]['short'], data[id]['size'], data[id]['url'], data[id]['status'], data[id]['uuid']), 200)
+''' % (new_board.date, new_board.digest, new_board.short, new_board.size, new_board.url, status, new_board.uuid), 200 if status == 'created' else 403)
         
 class RUDResource(Resource):
     def delete(self, id):
-        if not id in data:
-            response = make_response('Failed: Cannot find the UUID\n', 404)
-        else:
-            response = make_response('deleted %s\n' % id, 200)
-            del data[id]
-        return response
+        with get_session() as session:
+            board = session.query(Clipboard).filter_by(**dict(uuid = id)).first()
+            if board is None:
+                response = make_response('Failed: Cannot find the UUID\n', 404)
+            else:
+                response = make_response('deleted %s\n' % id, 200)
+                session.delete(board)
+                session.commit()
+            return response
     
     def put(self, id):
-        if not id in data:
-            response = make_response('Failed: Cannot find the UUID\n', 404)
-        else:
-            response = make_response('%s updated\n' % data[id]['url'], 200)
-            bytes = request.files['c'].read()
-            print('[Debug]Update the clipboard, content: %s' % bytes.decode())
-            md5 = hashlib.md5()
-            md5.update(bytes)
-            data[id]['content'] = bytes.decode()
-            data[id]['digest'] = md5.hexdigest()
-        return response
+        with get_session() as session:
+            board = session.query(Clipboard).filter_by(**dict(uuid = id)).first()
+            if board is None:
+                response = make_response('Failed: Cannot find the UUID\n', 404)
+            else:
+                response = make_response('%s updated\n' % board.url, 200)
+                bytes = request.files['c'].read()
+                print('[Debug]Update the clipboard, content: %s' % bytes.decode())
+                md5 = hashlib.md5()
+                md5.update(bytes)
+                board.content = bytes.decode()
+                board.digest = md5.hexdigest()
+                session.commit()
+            return response
     
     def get(self, id):
-        if not id in data:
-            response = make_response('Failed: Cannot find the UUID\n', 404)
-        else:
-            response = make_response(data[id]['content'], 200)
-        return response
-            
+        with get_session() as session:
+            board = session.query(Clipboard).filter_by(**dict(uuid = id)).first()
+            if board is None:
+                response = make_response('Failed: Cannot find the UUID\n', 404)
+            else:
+                response = make_response(board.content, 200)
+            return response
+          
 api.add_resource(CreateResource, '/')
 api.add_resource(RUDResource, '/<id>')
